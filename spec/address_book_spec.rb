630f935438225e11b4f0851f059c21d7fbffadf9
sean21502 <seanhite@gmail.com>
2017-08-05 15:05:37 -0400

    Update and rename address_book.spec.rb to address_book_spec.rb

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,22 +18,22 @@ RSpec.describe AddressBook do
      end
    end
 
-  describe "#add_entry" do
+  context "#remove_entry" do
+    it "removes entry from address book" do
+      book = AddressBook.new
+      entries = book.add_entry('John Doe', '123.456.7890', "johndoe@yahoo.com")
+
+      expect(book.entries.size).to eq(1)
+      book.remove_entry('John Doe', '123.456.7890', "johndoe@yahoo.com")
+
+      expect(book.entries.size).to eq(0)
+    end
+  end
+
+  context "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
       expect(book.entries.size).to eq(1)
     end
-
-    it "adds the correct information to entries" do
-      book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-      new_entry = book.entries[0]
-
-      expect(new_entry.name).to eq('Ada Lovelace')
-      expect(new_entry.phone_number).to eq('010.012.1815')
-      expect(new_entry.email).to eq('augusta.king@lovelace.com')
-    end
-  end
-end
